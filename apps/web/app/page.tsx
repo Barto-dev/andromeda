82c6c7b3f28192ff9f8ed515e56948fc1f4555e1
illia <illya1004work@gmail.com>
2025-10-03 20:01:39 +0100

    feat: update development server port and integrate Convex provider

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@workspace/ui/components/button";
 import { Input } from "@workspace/ui/components/input";
 import { add } from "@workspace/math/add";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  console.log(api);
   return (
     <div className="flex min-h-svh items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4">
@@ -13,6 +15,7 @@ export default function Page() {
           type="text"
           placeholder="Enter your name"
         />
+        wqw
       </div>
     </div>
   );
